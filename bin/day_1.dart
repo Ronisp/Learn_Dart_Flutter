c26af2f7834c3f54140e59c7f8e0f17bf695c647
Ronisp <Salauddin2205@gmail.com>
2022-03-31 16:13:49 +0600

    Done

---
+++ bin/day_1.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Data type
+
+  int variableNameID = 10;
+  String variableNameUserName = 'roni';
+  double cgpa = 3.3;
+  bool isUserActive = true;
+  List userName = [];
+  Map map = {};
+
+
+}
